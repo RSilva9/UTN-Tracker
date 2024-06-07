e45466f0054e1f1fe806981f4207290d088bae63
sarddo <ramasilva909@gmail.com>
2024-06-07 02:07:13 -0300

    Prototipo flechas
    Funcion de mostrar correlativas implementada. Falta arrelgar un par de asignación de clases cuando se tocan los checkbox.

--- src/components/Materias.js
+++ src/components/Materias.js
@@ -1,27 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react';
+import LeaderLine from 'leader-line-new';
 
-function Materias(){
+function Materias() {
     const [materias, setMaterias] = useState([])
     const [cursadas, setCursadas] = useState(JSON.parse(localStorage.getItem("cursadas")) || [])
     const [aprobadas, setAprobadas] = useState(JSON.parse(localStorage.getItem("aprobadas")) || [])
-    const niveles = [1, 2, 3, 4, 5];
+    const niveles = [1, 2, 3, 4, 5]
+    const linesRef = useRef({})
 
-    useEffect(()=>{
+    useEffect(() => {
         getMaterias()
     }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         localStorage.setItem("cursadas", JSON.stringify(cursadas))
     }, [cursadas])
 
-    useEffect(()=>{
+    useEffect(() => {
         localStorage.setItem("aprobadas", JSON.stringify(aprobadas))
     }, [aprobadas])
+
+    const getMaterias = async () => {
+        const response = await fetch("/carreras/sistemas.json")
+        const fetchedMaterias = await response.json()
+        setMaterias(fetchedMaterias)
+    }
 
     useEffect(() => {
         let mats = document.getElementsByClassName("materia");
         const handleItemClick = (event) => {
-            const item = event.currentTarget
+            const item = event.currentTarget.parentElement.parentElement.parentElement
             if (item.dataset.open === "false") {
                 item.classList.add("expanded")
                 item.dataset.open = "true"
@@ -31,6 +39,7 @@ function Materias(){
             }
         }
         for (let item of mats) {
+            item = item.childNodes[1].childNodes[2].childNodes[1]
             item.addEventListener("click", handleItemClick)
         }
         return () => {
@@ -40,31 +49,25 @@ function Materias(){
         }
     }, [materias])
 
-    const getMaterias = async()=>{
-        const response = await fetch("/carreras/sistemas.json")
-        const fetchedMaterias = await response.json()
-        setMaterias(fetchedMaterias)
-    }
-
     const uncheckDepend = (numero, tipo) => {
-        if(tipo === "cursada"){
-            for(let mat of materias){ // Loopear todas las materias del programa
-                if(mat.cursadas.includes(numero)){ // Si la materia necesita tener cursada la materia "numero"
+        if (tipo === "cursada") {
+            for (let mat of materias) {
+                if (mat.cursadas.includes(numero)) {
                     setAprobadas(prevAprobadas => prevAprobadas.filter(n => n !== mat.numero))
                     setCursadas(prevCursadas => {
-                        const newCursadas = prevCursadas.filter(n => n !== mat.numero) // Elimina esa materia de cursadas
+                        const newCursadas = prevCursadas.filter(n => n !== mat.numero)
                         uncheckDepend(mat.numero, "cursada")
                         uncheckDepend(mat.numero, "aprobada")
                         return newCursadas
                     })
                 }
             }
-        }else{ // Loopear todas las materias del programa
-            for(let mat of materias){
-                if(mat.aprobadas.includes(numero)){ // Si la materia necesita tener aprobada la materia "numero"
+        } else {
+            for (let mat of materias) {
+                if (mat.aprobadas.includes(numero)) {
                     setCursadas(prevCursadas => prevCursadas.filter(n => n !== mat.numero))
                     setAprobadas(prevAprobadas => {
-                        const newAprobadas = prevAprobadas.filter(n => n !== mat.numero) // Elimina esa materia de aprobadas
+                        const newAprobadas = prevAprobadas.filter(n => n !== mat.numero)
                         uncheckDepend(mat.numero, "cursada")
                         uncheckDepend(mat.numero, "aprobada")
                         return newAprobadas
@@ -76,12 +79,12 @@ function Materias(){
 
     const toggleCursadas = (numero) => {
         setCursadas(prevCursadas => {
-            if(prevCursadas.includes(numero)){
-                setAprobadas(prevAprobadas=> prevAprobadas.filter(n => n !== numero))
+            if (prevCursadas.includes(numero)) {
+                setAprobadas(prevAprobadas => prevAprobadas.filter(n => n !== numero))
                 uncheckDepend(numero, "cursada")
                 uncheckDepend(numero, "aprobada")
-                return prevCursadas.filter(n => n !== numero) 
-            }else{
+                return prevCursadas.filter(n => n !== numero)
+            } else {
                 return [...prevCursadas, numero]
             }
         })
@@ -91,55 +94,143 @@ function Materias(){
         setAprobadas(prevAprobadas => {
             if (prevAprobadas.includes(numero)) {
                 uncheckDepend(numero, "aprobada")
-                return prevAprobadas.filter(n => n !== numero);
+                return prevAprobadas.filter(n => n !== numero)
             } else {
-                setCursadas(prevCursadas=> [...prevCursadas, numero])
-                return [...prevAprobadas, numero];
+                setCursadas(prevCursadas => [...prevCursadas, numero])
+                return [...prevAprobadas, numero]
             }
         })
-    };
+    }
 
-    const checker = (arr, target) => target.every(v => arr.includes(v));
+    const checker = (arr, target) => target.every(v => arr.includes(v))
+
+    const toggleArrows = (numero, evt) => {
+        const clickedMateria = document.getElementById(numero)
+        const materiasDOM = document.getElementsByClassName("materia")
+        const cursadasArrows = []
+        const aprobadasArrows = []
+
+        for (let mat of materias) {
+            if (mat.numero === numero) {
+                for (let curs of mat.cursadas) {
+                    const foundMateria = document.getElementById(curs)
+                    cursadasArrows.push(foundMateria)
+                }
+                for (let aprob of mat.aprobadas) {
+                    const foundMateria = document.getElementById(aprob)
+                    aprobadasArrows.push(foundMateria)
+                }
+            }
+            if(evt.target.checked){
+                const check = document.getElementById(`Flechas-${mat.numero}`)
+                if(check.id != evt.target.id){
+                    check.disabled = true
+                }
+            }else{
+                const check = document.getElementById(`Flechas-${mat.numero}`)
+                check.disabled = false
+            }
+        }
+
+        if (evt.target.checked) {
+            clickedMateria.classList.add("scale")
+            for(let mat of materiasDOM){
+                if (Number(mat.id) !== numero) {
+                    mat.classList.add("transparent")
+                }
+            }
+
+            const newLines = []
+            cursadasArrows.forEach(cursada => {
+                cursada.classList.add("scale")
+                cursada.classList.remove("transparent")
+                const line = new LeaderLine(cursada, clickedMateria, { color: '#7FFFD4', endPlug: 'arrow2', hide: true, dropShadow: true })
+                line.show('draw', { duration: 1000 })
+                newLines.push(line)
+            })
+
+            aprobadasArrows.forEach(aprobada => {
+                aprobada.classList.add("scale")
+                aprobada.classList.remove("transparent")
+                const line = new LeaderLine(aprobada, clickedMateria, { color: '#ADFF2F', endPlug: 'arrow2', hide: true, dropShadow: true })
+                line.show('draw', { duration: 1000 })
+                newLines.push(line)
+            })
+
+            linesRef.current[numero] = newLines;
+        } else {
+            clickedMateria.classList.remove("scale")
+            aprobadasArrows.forEach(aprobada => {
+                aprobada.classList.remove("scale")
+            })
+            cursadasArrows.forEach(cursada => {
+                cursada.classList.remove("scale")
+            })
+
+            Array.from(materiasDOM).forEach(mat => {
+                if (Number(mat.id) !== numero) {
+                    mat.classList.remove("transparent")
+                }
+            })
+
+            if (linesRef.current[numero]) {
+                linesRef.current[numero].forEach(line => {
+                    line.hide('draw', { duration: 500 })
+                    setTimeout(() => line.remove(), 500)
+                });
+                delete linesRef.current[numero]
+            }
+        }
+    }
 
     return (
         <div className="materiasContainer">
             {niveles.map(nivel => (
                 <div className='nivelBlock' key={nivel}>
-                    {/* <hr></hr> */}
                     <h2>{nivel}° AÑO</h2>
-                    <hr></hr>
+                    <hr/>
                     {materias.filter(mat => mat.nivel === nivel).map(mat => (
-                        <div 
-                            key={mat.numero} 
+                        <div
+                            key={mat.numero}
                             className={`materia ${cursadas.includes(mat.numero) ? "" : "no-cursada"} 
                             ${aprobadas.includes(mat.numero) ? "aprobada" : ""}
                             ${checker(cursadas, mat.cursadas) ? "d-block" : "unavailable"}
                             ${checker(aprobadas, mat.aprobadas) ? "d-block" : "unavailable"}
                             `}
+                            id={mat.numero}
                             data-open="false"
                         >
                             <h2>{mat.nombre}</h2>
                             <div className='d-flex'>
                                 <div className='me-4'>
                                     <label htmlFor={`Cursada-${mat.numero}`}>Cursada</label>
-                                    <input 
-                                        id={`Cursada-${mat.numero}`} 
-                                        name="Cursada" 
-                                        type='checkbox' 
-                                        onChange={() => toggleCursadas(mat.numero)} 
-                                        checked={cursadas.includes(mat.numero)} 
+                                    <input
+                                        id={`Cursada-${mat.numero}`}
+                                        name="Cursada"
+                                        type='checkbox'
+                                        onChange={() => toggleCursadas(mat.numero)}
+                                        checked={cursadas.includes(mat.numero)}
+                                        disabled={!(checker(cursadas, mat.cursadas) && checker(aprobadas, mat.aprobadas))}
+                                    />
+                                </div>
+                                <div className='me-4'>
+                                    <label htmlFor={`Aprobada-${mat.numero}`}>Aprobada</label>
+                                    <input
+                                        id={`Aprobada-${mat.numero}`}
+                                        name="Aprobada"
+                                        type='checkbox'
+                                        onChange={() => toggleAprobadas(mat.numero)}
+                                        checked={aprobadas.includes(mat.numero)}
                                         disabled={!(checker(cursadas, mat.cursadas) && checker(aprobadas, mat.aprobadas))}
                                     />
                                 </div>
                                 <div>
-                                    <label htmlFor={`Aprobada-${mat.numero}`}>Aprobada</label>
-                                    <input 
-                                        id={`Aprobada-${mat.numero}`} 
-                                        name="Aprobada" 
-                                        type='checkbox' 
-                                        onChange={() => toggleAprobadas(mat.numero)} 
-                                        checked={aprobadas.includes(mat.numero)} 
-                                        disabled={!(checker(cursadas, mat.cursadas) && checker(aprobadas, mat.aprobadas))}
+                                    <label htmlFor={`Flechas-${mat.numero}`}>Correlativas</label>
+                                    <input
+                                        id={`Flechas-${mat.numero}`}
+                                        name="Flechas"
+                                        type='checkbox'
+                                        onChange={(evt) => toggleArrows(mat.numero, evt)}
                                     />
                                 </div>
                             </div>
@@ -169,4 +260,4 @@ function Materias(){
     );
 }
 
-export default Materias
+export default Materias;
